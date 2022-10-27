6d02fe9244e4a7088f73f1e5e9c5898a1af26a3e
Dammy36 <113221009+Dammy36@users.noreply.github.com>
2022-10-27 15:38:11 +0100

    added the current temperature

--- src/app.js
+++ src/app.js
@@ -1,4 +1,21 @@
-let apiKey = "ft2ff28777530dba3dddb311o0464bef"
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query={lagos}&key={apiKey}`
-console.log(apiUrl);
+function displayTemperature(response) {
+    console.log(response.data);     
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city-temp");
+    let descriptions = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
 
+    windElement.innerHTML= response.data.wind.speed;
+    humidityElement.innerHTML = response.data.temperature.humidity;   
+    descriptions.innerHTML = response.data.condition.description;
+    cityElement.innerHTML = Math.round(response.data.city);
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    
+}
+
+
+let apiKey = "ft2ff28777530dba3dddb311o0464bef"
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=lagos&key=ft2ff28777530dba3dddb311o0464bef&units=metrics`
+
+axios.get(apiUrl).then(displayTemperature);
